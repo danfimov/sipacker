6212bd7e3c5614ed0e50d0e6392015916be7ef7c
danfimov <danfimov@users.noreply.github.com>
2026-04-05 03:31:46 +0000

    deploy: 81514672547b895c0923104860035f270c03e8f5

---
+++ assets/consts-CiP6DBcQ.js
@@ -0,0 +1,2 @@
+import{ao as a}from"./vendor-mui-core-CjGXCec9.js";const n={pack:a.shape({uuid:a.string,name:a.string,creationTime:a.number,date:a.string,thumbnail:a.string})},t={xmlVersion:"1.0",encoding:"utf-8",latestVersion:4},e="\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b";export{n as c,t as f,e as u};
+//# sourceMappingURL=consts-CiP6DBcQ.js.map
